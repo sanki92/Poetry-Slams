97049a89e5d297ae6a938e78dea88efc2b09102a
Shankyy <sankalpt92@gmail.com>
2022-03-07 08:14:31 +0530

    First Commit

---
+++ src/components/Authors.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+
+
+function Authors() {
+  var a = 1;
+  // AUTHOR FETCH
+  const [author, setauthor] = useState([])
+  const [authorprop, setauthorprop] = useState("")
+  const [loadedAuthor, setloadedAuthor] = useState(false)
+
+    const getAuthors = async () => {
+        const data = await fetch('https://poetrydb.org/author')
+        const parsedData = await data.json()
+        setauthor(parsedData.authors)
+        setloadedAuthor(true)
+    }
+    useEffect(() => getAuthors(),[])
+   const toggleNav = () =>{
+     let btn = document.getElementById("toggleBtn")
+     let nav = document.getElementById("authorNav")
+     let main = document.getElementById("main")
+     btn.classList.toggle("close")
+
+    const mediaQuery = window.matchMedia('(max-width: 450px)')
+    if(mediaQuery.matches){
+      nav.classList.toggle("slideNav2")
+      nav.style.background="#240b36d4"
+    }
+    else{
+    nav.classList.toggle("slideNav")
+    nav.style.background="transparent"
+
+    }
+
+
+    if(main.style.marginLeft==="15rem"){
+      main.style.marginLeft="0"
+    }
+    else{
+      main.style.marginLeft="15rem"
+    }
+
+   }
+   const [poems, setpoems] = useState([])
+   const fetchPoems = async (e)=>{
+     setauthorprop(e.target.outerText)
+     // console.log(e)
+      const data = await fetch(`https://poetrydb.org/author/${e.target.outerText.replace(/ /g, '%20')}`)
+      const parsedData = await data.json()
+      setpoems(parsedData)
+   }
+
+  //  POEMS FETCH
+
+   
+
+  return (
+    <div>
+      <div id='toggleBtn' className='menuBar' onClick={toggleNav}>
+        <span id='bar'></span>
+        <span id='bar'></span>
+        <span id='bar'></span>
+        </div>
+        <div id='authorNav' className='authorContainer '>
+             <div className='authorNavIn'>
+               {!loadedAuthor?<div><img className='loader' src='https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif' alt='loader'></img></div>:author.map((element)=>{
+                return(
+                <p  className='authorElement mx-2' onClick={fetchPoems} key={element} value={element}>{element}</p>
+                )
+              })}
+             </div>
+        </div>
+
+             {/* POEMS */}
+              <div id='main' className='poemContainer'>
+                <h1 id='header'><span>P</span>oems</h1>
+                <p>{authorprop===""?"":"by "+authorprop}</p>
+                  <div>
+                    {!loadedAuthor?<div><img className='loader' src='https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif' alt='loader'></img></div>:poems.map((element)=>{
+                      return(
+                        < div className='poem' key={element.lines[0]}>  
+                        <h4 className='mt-5 mb-3 heading'>{element.title}</h4>
+                         { element.lines.map((item)=>{
+                         return (<p className='lines' key={a++}>{item}</p>)
+                        })}
+                          <p>* * *</p>
+                        </div>
+                    
+                      )
+                    })}
+                  </div>
+
+                </div>
+
+
+    </div>
+  )
+}
+
+export default Authors
